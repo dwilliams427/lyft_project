1fa3d11dacc42fd0f75b734c9e6baba84d9e3272
Daniel <dlw1ll27@gmail.com>
2021-06-17 16:40:12 -0700

    adds fetch request following tutorial

--- javascript_post.js
+++ javascript_post.js
@@ -1,8 +1,37 @@
-const Http = new XMLHttpRequest();
-const url='https://jsonplaceholder.typicode.com/posts';
-Http.open("GET", url);
-Http.send();
+// const Http = new XMLHttpRequest();
+// const url='https://jsonplaceholder.typicode.com/posts';
+// Http.open("GET", url);
+// Http.send();
 
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
-}
+// Http.onreadystatechange = (e) => {
+//   console.log(Http.responseText)
+// }
+
+// fetch("https://lyft-interview-test.glitch.me")
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (result) {
+//     document.querySelector("#results").textContent = result;
+//   })
+//   .catch(function (error) {
+//     console.log("Error: " + error);
+//   });
+
+
+
+
+
+//FOLLOW A LONG THIS TUTORIAL: https://www.youtube.com/watch?v=4reqmEJ6ct4&t=322s
+// res = fetch("https://ipinfo.io/json")
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (myJson) {
+//     document.querySelector("#results").textContent = myJson.ip;
+//   })
+//   .then(function (error) {
+//     console.log(error);
+//   })
+
+
